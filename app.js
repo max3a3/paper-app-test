c3e959baf1c606414325eb539de4dbbce8c66bd0
minsikim <minsiua@gmail.com>
2018-03-10 19:29:16 +0900

    simple line

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
+const locals = {};
+const pug = require('electron-pug')({pretty: true}, locals);
 
 //Open Main window(first)
 app.on('ready', ()=>{
   let mainWindow = new BrowserWindow({
+    //do not show at first
     show: false,
-    width: 1000,
-    height: 1000
+    width: 800,
+    height: 800,
+    fullscreenWindowTitle: true,
+    icon: './'
   });
-  mainWindow.loadURL(path.join('file://', __dirname, 'index.pug'));
+  mainWindow.loadURL(path.join('file://', __dirname, 'main.pug'));
+  //show when ready to show
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show();
+  })
 });
--- draw.js
+++ draw.js
@@ -1,1 +1,34 @@
-var paper = require('paper-jsdom-canvas');
+var paper = require('paper');
+var canvas = document.getElementById('myCanvas');
+
+console.log(paper);
+paper.setup(canvas);
+console.log(canvas);
+var newpath = new paper.Path({
+  strokeColor: 'black'
+});
+var p1 = new paper.Point(100,100);
+var p2 = new paper.Point(200,200);
+newpath.add(p1);
+newpath.add(p2);
+// newpath.moveTo(p1);
+// newpath.lineTo(p2);
+console.log(newpath);
+console.log(p1);
+console.log(p2);
+canvas.on('click',function(){
+  onMouseDown();
+});
+function onMouseDown(event){
+  console.log('pressed!');
+}
+
+// canvas.offsetWidth = window.innerWidth;
+// canvas.offsetHeight = window.innerHeight;
+// canvas.setAttribute("width", window.innerWidth);
+// canvas.setAttribute("height", window.innerHeight);
+
+// var path = new paper.Path();
+// path.add(new paper.Point(100, 100));
+// path.add(new paper.Point(200, 200));
+// paper.view.draw(path);
