102b910a91dfc765c3678a290f8e991390e7b62e
Salinger MS <minsiua@gmail.com>
2018-03-16 20:02:25 +0900

    loading font test

--- drawfont.js
+++ drawfont.js
@@ -1,3 +1,39 @@
 const remote = require('electron').remote;
 var opentype = remote.getGlobal('opentype');
 console.log(opentype);
+
+var font;
+
+function loadFont(){
+    let font = opentype.Font.loadSync();
+}
+
+function onReadFile(e) {
+    // document.getElementById('font-name').innerHTML = '';
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        try {
+            var font = opentype.parse(e.target.result);
+            onFontLoaded(font);
+            // showErrorMessage('');
+        } catch (err) {
+            // showErrorMessage(err.toString());
+            if (err.stack) console.log(err.stack);
+	    throw(err);
+        }
+    };
+    reader.onerror = function(err) {
+        showErrorMessage(err.toString());
+    };
+    reader.readAsArrayBuffer(file);
+}
+
+function onFontLoaded(font) {
+    window.font = font;
+    // renderText();
+    // displayFontData();
+}
+
+var fileButton = document.getElementById('file');
+fileButton.addEventListener('change', onReadFile, false);
