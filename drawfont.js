fcc1dd4e528328abbf2300112f21a1854006b2f6
Salinger MS <minsiua@gmail.com>
2018-03-18 23:37:32 +0900

    style update 3

--- drawfont.js
+++ drawfont.js
@@ -7,11 +7,45 @@ console.log(opentype);
 let font;
 let fontFileName = 'fonts/SDGTM.ttf';
 
-loadFont(fontFileName);
+// ********************************** //
+// left menu tab controls             //
+// ********************************** //
 
-document.getElementById('load-default').addEventListener('click', function(){
-    loadFont(fontFileName);
-});
+let glyphTAB = document.getElementById('glyphs');
+let fontinfoTAB = document.getElementById('fontinfo');
+let fonttableTAB = document.getElementById('fonttable');
+let glyphMenu = document.getElementById('glyph-menu');
+let fontinfoMenu = document.getElementById('info-menu');
+let fonttableMenu = document.getElementById('table-menu');
+
+let clickTAB1 = function (){
+    console.log('glyphs TAB clicked');
+    glyphTAB.classList.add('selected');
+    glyphMenu.classList.remove('display-none');
+    fontinfoMenu.classList.add('display-none');
+    fonttableMenu.classList.add('display-none');
+};
+let clickTAB2 = function (){
+    console.log('font info TAB clicked');
+    fontinfoTAB.classList.add('selected');
+    fontinfoMenu.classList.remove('display-none');
+    glyphMenu.classList.add('display-none');
+    fonttableMenu.classList.add('display-none');
+};
+let clickTAB2 = function (){
+    console.log('font info TAB clicked');
+    fonttableTAB.classList.add('selected');
+    fonttableMenu.classList.remove('display-none');
+    glyphMenu.classList.add('display-none');
+    fontinfoMenu.classList.add('display-none');
+};
+
+glyphTAB.addEventListener("click", clickTAB1, false);
+fontinfoTAB.addEventListener("click", clickTAB2, false);
+fonttableTAB.addEventListener("click", clickTAB3, false);
+
+
+loadFont(fontFileName);
 
 function loadFont(fontFileName){
     font = opentype.loadSync(fontFileName);
@@ -29,38 +63,46 @@ function listAll(o){
     }
 }
 
-function onReadFile(e) {
-    // document.getElementById('font-name').innerHTML = '';
-    var temp = e.target.files[0];
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        try {
-            console.log('try');
-            var font = opentype.parse(e.target.result);
-            onFontLoaded(font);
-            // showErrorMessage('');
-        } catch (err) {
-            console.log('catch');
-            // showErrorMessage(err.toString());
-            if (err.stack) console.log(err.stack);
-        throw(err);
-        console.log('throw');
-        }
-    };
-    reader.onerror = function(err) {
-        showErrorMessage(err.toString());
-    };
-    reader.readAsArrayBuffer(temp);
-}
+// document.getElementById('load-default').addEventListener('click', function(){
+//     loadFont(fontFileName);
+// });
 
-function onFontLoaded(font) {
-    window.font = font;
-    // renderText();
-    // displayFontData();
-}
 
-var fileButton = document.getElementById('file');
-fileButton.addEventListener('change', onReadFile, false);
+
+
+
+// function onReadFile(e) {
+//     // document.getElementById('font-name').innerHTML = '';
+//     var temp = e.target.files[0];
+//     var reader = new FileReader();
+//     reader.onload = function(e) {
+//         try {
+//             console.log('try');
+//             var font = opentype.parse(e.target.result);
+//             onFontLoaded(font);
+//             // showErrorMessage('');
+//         } catch (err) {
+//             console.log('catch');
+//             // showErrorMessage(err.toString());
+//             if (err.stack) console.log(err.stack);
+//         throw(err);
+//         console.log('throw');
+//         }
+//     };
+//     reader.onerror = function(err) {
+//         showErrorMessage(err.toString());
+//     };
+//     reader.readAsArrayBuffer(temp);
+// }
+
+// function onFontLoaded(font) {
+//     window.font = font;
+//     // renderText();
+//     // displayFontData();
+// }
+
+// var fileButton = document.getElementById('file');
+// fileButton.addEventListener('change', onReadFile, false);
 
 //index selection toggle
 var indexButtons = document.querySelectorAll('.index-button')
@@ -73,14 +115,8 @@ document.querySelectorAll('.index-selected').addEventListener('click', ()=>{
     this.classList.remove('index-selected');
 }, false);
 //add Listeners to Left menu tabs
-let glyphTAB = document.getElementById('glyphs');
-var fontinfoTAB = document.getElementById('fontinfo');
-var fonttable = document.getElementById('fonttable');
-function clickTAB1 (){
-    console.log('glyphTAB clicked');
-    glyphTAB.classList.add('selected');
-};
-glyphTAB.addEventListener("click", clickTAB1, false);
+
+
 // glyphTAB.onclick = function(){
 //     console.log('glyphTAB clicked');
 // }
